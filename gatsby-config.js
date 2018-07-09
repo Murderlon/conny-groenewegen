bdfbd6c9d7d541cfb9116d5f97ff9aed72cb6715
Murderlon <hello@murderlon.me>
2018-07-09 22:51:36 +0200

    add svg-react-loader, import & use logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,7 @@ module.exports = {
     `gatsby-plugin-netlify-cms`,
     'gatsby-plugin-eslint',
     `gatsby-plugin-react-next`,
-    `gatsby-plugin-styled-components`
+    `gatsby-plugin-styled-components`,
+    'gatsby-plugin-react-svg'
   ]
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,21 @@
 import React from 'react'
+import styled from 'styled-components'
 
-const Index = () => <h1>Hello world!</h1>
+import LogoSVG from '../icons/logo.svg'
+import Microscope from '../components/Microscope'
+
+const Logo = styled(LogoSVG)`
+  color: ${p => p.theme.white};
+`
+const Header = styled.header`
+  margin: ${p => p.theme.spacing.medium};
+`
+
+const Index = () => (
+  <Header>
+    <Logo />
+    <Microscope />
+  </Header>
+)
 
 export default Index
