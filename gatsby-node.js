18bbee691890c935ace2f9f192e7810e0fa03212
Murderlon <hello@murderlon.me>
2018-07-14 23:26:04 +0200

    create project template, change cms settings

---
+++ gatsby-node.js
@@ -0,0 +1,59 @@
+const path = require('path')
+const { createFilePath } = require('gatsby-source-filesystem')
+
+exports.createPages = ({ boundActionCreators, graphql }) => {
+  const { createPage } = boundActionCreators
+
+  return graphql(`
+    {
+      allMarkdownRemark(limit: 1000) {
+        edges {
+          node {
+            id
+            fields {
+              slug
+            }
+            frontmatter {
+              templateKey
+            }
+          }
+        }
+      }
+    }
+  `).then(result => {
+    if (result.errors) {
+      // eslint-disable-next-line no-console
+      result.errors.forEach(e => console.error(e.toString()))
+      return Promise.reject(result.errors)
+    }
+
+    const posts = result.data.allMarkdownRemark.edges
+
+    posts.forEach(edge => {
+      const id = edge.node.id
+      createPage({
+        path: edge.node.fields.slug,
+        component: path.resolve(
+          `src/templates/${String(edge.node.frontmatter.templateKey)}.js`
+        ),
+        // additional data can be passed via context
+        context: {
+          id
+        }
+      })
+    })
+  })
+}
+
+exports.onCreateNode = ({ node, boundActionCreators, getNode }) => {
+  const { createNodeField } = boundActionCreators
+
+  if (node.internal.type === `MarkdownRemark`) {
+    const value = createFilePath({ node, getNode })
+    createNodeField({
+      name: `slug`,
+      node,
+      value
+    })
+  }
+}
---
+++ src/templates/project-detail.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+export default function Template({ data }) {
+  const { markdownRemark } = data
+  const { frontmatter, html, id } = markdownRemark
+  const { title } = frontmatter
+  console.log(data)
+
+  // meh
+  const parent = id.includes('couture')
+    ? 'Couture'
+    : id.includes('space')
+      ? 'Space'
+      : 'Materialize'
+
+  return (
+    <main>
+      <Link to={`/${parent.toLocaleLowerCase()}`}>Re-{parent}</Link>
+      <h1>{title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: html }} />
+    </main>
+  )
+}
+
+export const pageQuery = graphql`
+  query ProjectPostByID($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      id
+      html
+      frontmatter {
+        title
+        description
+        headerImage
+        images
+      }
+    }
+  }
+`
