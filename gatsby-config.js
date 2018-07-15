5510edd0de36c313743c8fb36fe8d67a5c2fbb36
Murderlon <hello@murderlon.me>
2018-07-15 16:26:39 +0200

    add gatsby-image and sharp plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,26 @@
 module.exports = {
   plugins: [
-    `gatsby-plugin-netlify-cms`,
-    'gatsby-plugin-eslint',
     `gatsby-plugin-react-next`,
     `gatsby-plugin-styled-components`,
-    'gatsby-plugin-react-svg',
+    `gatsby-plugin-netlify-cms`,
+    'gatsby-plugin-eslint',
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/src/pages`,
+        path: `${__dirname}/src/pages/`,
         name: 'pages'
       }
     },
-    `gatsby-transformer-remark`
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `img`,
+        path: `${__dirname}/src/images/`
+      }
+    },
+    `gatsby-transformer-remark`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    'gatsby-plugin-react-svg'
   ]
 }
