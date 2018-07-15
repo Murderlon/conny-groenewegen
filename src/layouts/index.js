3acd38bf332b5d88dd99949a8688e9ff7fd62e18
Murderlon <hello@murderlon.me>
2018-07-15 19:30:30 +0200

    move header/logo from index to layout

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,7 +1,8 @@
 import React, { Fragment } from 'react'
-import { injectGlobal, ThemeProvider } from 'styled-components'
+import styled, { injectGlobal, ThemeProvider } from 'styled-components'
 import { func } from 'prop-types'
 
+import LogoSVG from '../icons/logo.svg'
 import base from '../styles/global/base'
 import typography from '../styles/global/typography'
 import variables from '../styles/themes/variables'
@@ -12,9 +13,26 @@ injectGlobal`
   ${typography}
 `
 
+const Main = styled.main`
+  margin: ${p => p.theme.spacing.medium};
+`
+const Logo = styled(LogoSVG)`
+  color: ${p => p.theme.white};
+`
+const Header = styled.header`
+  margin: ${p => p.theme.spacing.xlarge} 0;
+  display: flex;
+  justify-content: center;
+`
+
 const TemplateWrapper = ({ children }) => (
   <ThemeProvider theme={{ ...colors, ...variables }}>
-    <Fragment>{children()}</Fragment>
+    <Fragment>
+      <Header>
+        <Logo />
+      </Header>
+      <Main>{children()}</Main>
+    </Fragment>
   </ThemeProvider>
 )
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,25 @@
 import React, { Fragment } from 'react'
-import styled from 'styled-components'
 
-import LogoSVG from '../icons/logo.svg'
 import Microscope from '../components/Microscope'
-
-const Logo = styled(LogoSVG)`
-  color: ${p => p.theme.white};
-`
-const Header = styled.header`
-  margin: ${p => p.theme.spacing.xlarge} 0;
-  display: flex;
-  justify-content: center;
-`
-
-const Main = styled.main`
-  margin: ${p => p.theme.spacing.medium};
-`
 
 const Index = () => (
   <Fragment>
-    <Header>
-      <Logo />
-    </Header>
-    <Main>
-      <Microscope />
-    </Main>
+    <Microscope />
+    <h2>Collectives</h2>
+    <section>
+      <h3>FaMa</h3>
+      <p>
+        Fashion Machine (FaMa) is commenting on and asking critical questions
+        about the (waste) production of the worldwide clothing industry.
+      </p>
+    </section>
+    <section>
+      <h3>Electric Co.</h3>
+      <p>
+        Process driven research revealing the constructive potential of soft
+        tech materials.
+      </p>
+    </section>
   </Fragment>
 )
 
