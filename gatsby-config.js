95653d02cd72987ccb74eae9102bce2da0d0d353
Murderlon <hello@murderlon.me>
2018-07-21 19:38:15 +0200

    use proper quotes in gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,26 +1,26 @@
 module.exports = {
   plugins: [
-    `gatsby-plugin-react-next`,
-    `gatsby-plugin-styled-components`,
-    `gatsby-plugin-netlify-cms`,
+    'gatsby-plugin-react-next',
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-netlify-cms',
     'gatsby-plugin-eslint',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/src/pages/`,
         name: 'pages'
       }
     },
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `img`,
+        name: 'img',
         path: `${__dirname}/src/images/`
       }
     },
-    `gatsby-transformer-remark`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    'gatsby-transformer-remark',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     'gatsby-plugin-react-svg'
   ]
 }
