953e02213b71f6017906a202d1f35c59a3514226
Murderlon <hello@murderlon.me>
2018-07-21 18:06:07 +0200

    setup basic project-detail page

--- src/templates/project-detail.js
+++ src/templates/project-detail.js
@@ -1,24 +1,44 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import styled from 'styled-components'
+
+import Button from '../components/Button'
+
+const ImageGrid = styled.section`
+  display: flex;
+  margin: ${({ theme }) => theme.spacing.large} 0;
+`
+
+const Img = styled.img`
+  flex: 1;
+`
 
 export default function Template({ data }) {
-  const { markdownRemark } = data
-  const { frontmatter, html, id } = markdownRemark
-  const { title } = frontmatter
-  console.log(data)
-
-  // meh
-  const parent = id.includes('couture')
-    ? 'Couture'
-    : id.includes('space')
-      ? 'Space'
-      : 'Materialize'
+  const {
+    title,
+    headerImage,
+    description,
+    images
+  } = data.markdownRemark.frontmatter
+  console.log(images)
 
   return (
     <main>
-      <Link to={`/${parent.toLocaleLowerCase()}`}>Re-{parent}</Link>
+      <Link to="/re-couture">Re-Couture</Link>
       <h1>{title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: html }} />
+
+      <img src={headerImage.image} />
+      {description && <p>{description}</p>}
+
+      <ImageGrid>
+        {images.map(({ image, alt }, i) => (
+          <Img src={image} alt={alt} key={i} />
+        ))}
+      </ImageGrid>
+
+      <Button to="" labelText="Previous project">
+        Primary Explores
+      </Button>
     </main>
   )
 }
@@ -27,9 +47,17 @@ export const pageQuery = graphql`
   query ProjectPostByID($id: String!) {
     markdownRemark(id: { eq: $id }) {
       id
-      html
       frontmatter {
         title
+        headerImage {
+          alt
+          image
+        }
+        description
+        images {
+          alt
+          image
+        }
       }
     }
   }
