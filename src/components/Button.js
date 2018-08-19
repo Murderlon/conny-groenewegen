86f1eb4658aeaacc854a99359879aabd4e7297e2
Murderlon <hello@murderlon.me>
2018-08-19 15:09:39 +0200

    Improve Button

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,10 +3,12 @@ import GatsbyLink from 'gatsby-link'
 import styled from 'styled-components'
 
 const Link = styled(GatsbyLink)`
-  padding: ${({ theme }) => theme.spacing.medium};
+  padding: ${({ theme }) => `${theme.spacing.small} ${theme.spacing.medium}`};
   border: 1px solid ${({ theme }) => theme.white};
   border-radius: 3px;
   font-family: 'IBM Plex Mono, mono';
+  text-transform: uppercase;
+  display: inline-block;
 `
 
 const Label = styled.span`
@@ -16,11 +18,25 @@ const Label = styled.span`
   font-size: 0.8em;
 `
 
-const Button = ({ labelText, children, to }) => (
-  <Link to={to}>
-    {labelText && <Label>{labelText}</Label>}
-    {children}
-  </Link>
-)
+const Button = ({ labelText, children, to }) => {
+  const internal = /^\/(?!\/)/.test(to)
+
+  // Use gatsby-link for internal links, and <a> for others
+  if (internal) {
+    return (
+      <Link to={to}>
+        {labelText && <Label>{labelText}</Label>}
+        {children}
+      </Link>
+    )
+  }
+  const ExternalLink = Link.withComponent('a')
+  return (
+    <ExternalLink href={to}>
+      {labelText && <Label>{labelText}</Label>}
+      {children}
+    </ExternalLink>
+  )
+}
 
 export default Button
