063c0b5b61946c7f92a19bb676b9adf7b3a11445
Murderlon <hello@murderlon.me>
2018-08-26 15:06:39 +0200

    add temporary text in CategoryHeader

--- src/components/CategoryHeader.js
+++ src/components/CategoryHeader.js
@@ -42,7 +42,13 @@ const CategoryHeader = ({ title, color }) => {
         re-
         <span>{title}</span>
       </Heading>
-      <p>[needs explanation]</p>
+      <p>
+        {' '}
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat.
+      </p>
     </Fragment>
   )
 }
