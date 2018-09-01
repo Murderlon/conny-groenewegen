edce7f1aee387e077a4501829d9d3783701481cc
Murderlon <hello@murderlon.me>
2018-09-01 18:40:26 +0200

    add shadow to MaskedVideo

--- src/components/MaskedVideo.js
+++ src/components/MaskedVideo.js
@@ -5,10 +5,24 @@ const VideoWrapper = styled.div`
   margin-right: ${({ theme }) => theme.spacing.small};
 
   div {
+    position: relative;
     width: 5em;
     height: 5em;
     border-radius: 50%;
     overflow: hidden;
+
+    &::after {
+      position: absolute;
+      left: 0;
+      top: 0;
+      content: '';
+      width: 100%;
+      height: 100%;
+      box-shadow: inset 0 0 20px ${({ theme }) => theme.black},
+        inset 0 0 10px ${({ theme }) => theme.black},
+        inset 0 0 5px ${({ theme }) => theme.black};
+      border-radius: 50%;
+    }
   }
 
   video {
