3dee868a2ad9693ac6cb1e0ff31ef4bc14098e9f
daanrongen <daanrongen@hotmail.com>
2018-10-06 19:24:42 +0200

    remove unused colors

--- src/styles/themes/colors.js
+++ src/styles/themes/colors.js
@@ -2,11 +2,6 @@ const colors = {
   black: 'black',
   white: '#FEFEFE',
   grey: '#DFDFDE',
-  yellow: '#fffd38',
-  cyan: '#2dfffe',
-  magenta: '#fd28fc',
-  red: '#ef3e36',
-  green: '#34d1bf',
   spaceColor: '#babaa0',
   coutureColor: '#fc281e',
   materializeColor: '#b5b5b5'
